c34e217e2342dd2d60abc7d1591c2e387c39554f
“pramod” <c10.promod@gmail.com>
2023-02-12 03:28:09 +0545

    return Statement in JavaScript Function

---
+++ 5.Fuctions/h_returnStatement.js
@@ -0,0 +1,12 @@
+/**
+ * Return Statement
+ * => A return statement may be return any type data, including arrays and objects.
+ * Syntax:
+ *  return (varialbe or expression);
+ */
+function multi(a, b) {
+    return (a * b);
+}
+console.log(multi(5, 9))
+console.log(multi(4, 8))
+console.log(multi(3, 7))
