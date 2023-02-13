a13cedf3279cd047cfea388eb3c9c9e0ffbb9771
“pramod” <c10.promod@gmail.com>
2023-02-13 17:47:48 +0545

    Block Scope with let and const where var is global inside or outside block

---
+++ 5.Fuctions/j_blockScope.js
@@ -0,0 +1,17 @@
+/**
+ * Block Scope
+ * => 
+ */
+
+// Block Scope Example
+if (true) {
+    var i = 10;
+    let j = 20;
+    const k = 30;
+    console.log(i)
+    console.log(j)
+    console.log(k)
+}
+console.log(i)
+// console.log(j)
+// console.log(k)
