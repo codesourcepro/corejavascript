363809446a0432feed1ab3ff034629900fda5338
“pramod” <c10.promod@gmail.com>
2023-02-14 03:00:51 +0545

    Function Expression declare with variable having no hoisting support

---
+++ 5.Fuctions/m_functionExpression.js
@@ -0,0 +1,24 @@
+/**
+ * Function Expression
+ * => When we create a function and assign it to a variable, known as function expression.
+ * Note: You can't call function expression before function definition.
+ *       Function expressions in jS are not hoisted, unlike function declarations.
+ * 
+ * 
+ */
+
+// Example;
+
+// Function declaration
+show();
+function show() {
+    console.log("Function Declaration");
+}
+show();
+
+// Function Expression
+// disp();
+var disp = function show() {
+    console.log("Function Expression");
+}
+disp();
